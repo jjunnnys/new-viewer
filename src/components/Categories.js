8466202478d9b2c9811bf69069b88f4085ba1695
ppby <salswnss@gmail.com>
2020-07-13 10:52:38 +0900

    Setting NavLink

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const categories = [
   {
@@ -43,7 +44,8 @@ const CategoriesBlock = styled.div`
   }
 `;
 
-const Category = styled.div`
+// NavLink에 스타일 추가
+const Category = styled(NavLink)`
   font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
@@ -55,31 +57,34 @@ const Category = styled.div`
     color: #490557;
   }
 
-  /* 프롭스로 받는 active가 true면 밑에 코드 실행 */
-  ${(props) =>
-    props.active &&
-    css`
-      font-weight: 600;
-      border-bottom: 2px solid #22b8cf;
-      color: #22b8cf;
-      &:hover {
-        color: #3bc9db;
-      }
-    `}
+  /* Category+클래스 네임 */
+  &.active {
+    font-weight: 700;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &:hover {
+      color: #3bc9db;
+    }
+  }
 
   & + & {
     margin-left: 1rem;
   }
 `;
 
-const Categories = ({ onSelect, category }) => {
+const Categories = () => {
   return (
     <CategoriesBlock>
       {categories.map((c) => (
         <Category
           key={c.name}
-          active={category === c.name}
-          onClick={() => onSelect(c.name)}
+          activeClassName="active" // active될 때 클래스 네임
+          exact={c.name === 'all'}
+          /* 
+            to 값이 '/' 을 가리키고 있을 때 true로 만듬, 
+            만약 이 값을 설정하지 않으면, 다른 카테고리가 선택되었을 떄도 전체보기 링크에 active 스타일이 저굥ㅇ되는 오류 발생 
+          */
+          to={c.name === 'all' ? '/' : `/${c.name}`}
         >
           {c.text}
         </Category>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BorowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BorowserRouter>
+    <BrowserRouter>
       <App />
-    </BorowserRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
