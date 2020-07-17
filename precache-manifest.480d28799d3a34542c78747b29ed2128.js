6a91e9f8c6055e3dc3217afc7725d7da43005262
ppby <salswnss@gmail.com>
2020-07-17 23:49:42 +0900

    Updates

--- precache-manifest.480d28799d3a34542c78747b29ed2128.js
+++ precache-manifest.480d28799d3a34542c78747b29ed2128.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b16567f3001b603e6c227fed4152d6a9",
+    "revision": "a4026db2366ece253a6cd05360f8824e",
     "url": "/news-viewer/index.html"
   },
   {
-    "revision": "3cdfec3baa119618f003",
+    "revision": "73d118ff8eb467ebcea7",
     "url": "/news-viewer/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/news-viewer/static/js/2.dfd3f2f5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3cdfec3baa119618f003",
-    "url": "/news-viewer/static/js/main.0aac3299.chunk.js"
+    "revision": "73d118ff8eb467ebcea7",
+    "url": "/news-viewer/static/js/main.4b5594f0.chunk.js"
   },
   {
     "revision": "ef96a0caf70099cc53ce",
